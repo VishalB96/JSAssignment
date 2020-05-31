498e0a8e879c6847456070d687b50efc445890b2
VishalB96 <vsb12vsb12@gmail.com>
2020-05-31 13:20:33 +0530

    task 2 update

---
+++ JS_MINI_PROJECTS/js/task2.js
@@ -0,0 +1,24 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    alert(timer);
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            document.getElementById("op1").innerHTML = "TIME OUT";
+            document.getElementById("op1").style.fontSize = "10em";
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 1,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
