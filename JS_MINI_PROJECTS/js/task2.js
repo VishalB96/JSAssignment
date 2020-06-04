c2dde7eb671ab8d5b997b7c124fa6be9731ae379
VishalB96 <vsb12vsb12@gmail.com>
2020-06-04 13:33:51 +0530

    task 2 updated

--- JS_MINI_PROJECTS/js/task2.js
+++ JS_MINI_PROJECTS/js/task2.js
@@ -1,12 +1,12 @@
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    alert(timer);
+    //alert(timer);
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;//to add 0 in 10s place
+        seconds = seconds < 10 ? "0" + seconds : seconds;//to add 0 in 10s place
 
         display.textContent = minutes + ":" + seconds;
 
